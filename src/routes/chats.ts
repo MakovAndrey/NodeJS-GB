6c2e02aa98577a055e5d24cf4bac33e4168b264e
MakovAndrey <mayc52@mail.ru>
2022-11-01 19:38:52 +0300

    lesson-6

---
+++ src/routes/chats.ts
@@ -0,0 +1,34 @@
+import express, { Request } from 'express';
+import { Chats } from '../models/chats';
+
+export interface TypedRequestBody<T> extends Request {
+    body: T;
+}
+
+const router = express.Router();
+
+router
+    .get('/', async (_, res) => {
+        const chats = await Chats.find();
+        res.json(chats);
+    })
+    .post('/', async (req: TypedRequestBody<{ name: string }>, res, next) => {
+        await Chats.create(req.body, (err: Error, newChat: typeof Chats) => {
+            if (err) {
+                next(err);
+            }
+
+            res.status(201);
+            res.json(newChat);
+        });
+    })
+    .delete('/:id', async (req, res) => {
+        const deleted = await Chats.findByIdAndDelete(req.params.id);
+        res.json(deleted);
+    })
+    .put('/:id', async (req, res) => {
+        const updateChat = await Chats.findByIdAndUpdate(req.params.id, req.body);
+        res.json(updateChat);
+    });
+
+export default router;
